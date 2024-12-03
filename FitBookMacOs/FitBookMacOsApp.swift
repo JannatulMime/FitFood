29c95bc26b4a3459eb87439708bbfa2abe0ea57d
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-03 18:32:41 +0600

    Add MacOs target

---
+++ FitBookMacOs/FitBookMacOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBookMacOsApp.swift
+//  FitBookMacOs
+//
+//  Created by Habibur Rahman on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitBookMacOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
