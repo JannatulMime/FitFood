f452923eeb501101f31f384ed03eb77b08c850ad
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-12-07 10:36:06 +0600

    Add FitFood Core for all targets

--- FitFood/Auth/FirebaseAuthManager.swift
+++ FitFood/Auth/FirebaseAuthManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import FirebaseHelper
-
+import FitFoodCore
 
 class FirebaseAuthManager {
     
---
+++ FitFoodCore/FitFoodCore.h
@@ -0,0 +1,18 @@
+//
+//  FitFoodCore.h
+//  FitFoodCore
+//
+//  Created by Habibur Rahman on 7/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FitFoodCore.
+FOUNDATION_EXPORT double FitFoodCoreVersionNumber;
+
+//! Project version string for FitFoodCore.
+FOUNDATION_EXPORT const unsigned char FitFoodCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FitFoodCore/PublicHeader.h>
+
+
