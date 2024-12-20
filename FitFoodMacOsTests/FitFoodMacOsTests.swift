699741f4e8739941d5d901abb19a8d6fa4cc0f04
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-20 21:58:36 +0600

    Add imagekit image uploader

---
+++ FitFoodMacOsTests/FitFoodMacOsTests.swift
@@ -0,0 +1,16 @@
+//
+//  FitFoodMacOsTests.swift
+//  FitFoodMacOsTests
+//
+//  Created by Habibur Rahman on 20/12/24.
+//
+
+import XCTest
+
+final class FitFoodMacOsTests: XCTestCase {
+
+    func test_isfail(){
+        XCTFail("fail")
+    }
+
+}
